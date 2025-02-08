fa908dc4a94536d45db4908d391bd5b9ccad0597
Gangurdedhruv <gangurdedhruv1519@gmail.com>
2025-02-09 00:37:42 +0530

    api key hiding

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -4,14 +4,15 @@ import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import HomePage from "./Pages/Homepage";
 
+
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/homepage" element={<HomePage />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/homepage" element={<HomePage />} />
+    </Routes>
   );
 }
 
