6597e128a6592f5f221e138b79517b912c4ef3a4
pagasis <74396469+Pagasis@users.noreply.github.com>
2025-02-09 23:21:54 +0530

    compiler component

---
+++ frontend/src/components/Compiler.jsx
@@ -0,0 +1,13 @@
+const Compiler = () => {
+    return (
+    <div className="">
+        <iframe
+            height="450px"  
+            src="https://onecompiler.com/embed?hideNew=true&hideNewFileOption=true&hideTitle=true&disableCopyPaste=true&disableAutoComplete=true&theme=dark&codeChangeEvent=true&hideResult=true" 
+            width="100%"
+        ></iframe>
+    </div>
+    )
+}
+
+export default Compiler
