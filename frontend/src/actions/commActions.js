d5bd4fd35e94cfe4c94f2773289a0164316a63a4
pagasis <74396469+Pagasis@users.noreply.github.com>
2025-03-14 21:49:58 +0530

    comm actions

--- frontend/src/actions/commActions.js
+++ frontend/src/actions/commActions.js
@@ -1,13 +1,29 @@
-export const getAllPosts = () => {
-    return fetch(`http://localhost:5000/api/comm/get-all-posts`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .catch((err) => console.log("Error getting posts:", err));
+export const getAllPosts = async () => {
+    try {
+        const response = await fetch(`http://localhost:5000/api/comm/get-all-posts`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
+        return await response.json();
+    } catch (err) {
+        return console.log("Error getting posts:", err);
+    }
 };
+
+export const getPostById = async (id) => {
+    try {
+        const response = await fetch(`http://localhost:5000/api/comm/get-post-by-id/${id}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
+        return await response.json();
+    } catch (err) {
+        return console.log("Error getting posts:", err);
+    }
+}
