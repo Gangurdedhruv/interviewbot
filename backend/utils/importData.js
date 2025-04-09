ec7ad8f8bbc84f1fe121149ddc5105b0f790c5f4
pagasis <74396469+Pagasis@users.noreply.github.com>
2025-04-09 21:20:44 +0530

    url change

--- backend/utils/importData.js
+++ backend/utils/importData.js
@@ -10,7 +10,7 @@ dotenv.config({
 })
 
 try {
-    const conn = await mongoose.connect(process.env.MONGO_LOCAL_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
 } catch (error) {
     console.error(`Error: ${error.message}`);
